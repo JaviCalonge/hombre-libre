bd062ee9fff765f3b44a18d80db385625a5f7702
Javi Calonge <javicalongem@gmail.com>
2024-11-01 16:49:38 +0100

    Cambiar ruta imágenes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer>&#169 2024 Hombre Libre
-    <a href="x.com"><img class="icon" src="/src/img/x.png" alt="Enlace a X" width="14" height="14"></a>
-    <a href="instagran.com"><img class="icon" src="/src/img/ig.png" alt="Enlace a X" width="14" height="14"></a>
+    <a href="x.com"><img class="icon" src="/public/img/x.png" alt="Enlace a X" width="14" height="14"></a>
+    <a href="instagran.com"><img class="icon" src="/public/img/ig.png" alt="Enlace a X" width="14" height="14"></a>
   </footer>
 </template>
